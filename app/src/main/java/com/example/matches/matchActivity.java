f6fa76df13d3c1a005f9bc3da8234b440f7b347c
Simon <simon.goo28@gmail.com>
2020-11-15 16:49:42 +0100

    matchActivity

--- app/src/main/java/com/example/matches/matchActivity.java
+++ app/src/main/java/com/example/matches/matchActivity.java
@@ -1,57 +1,54 @@
 package com.example.matches;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class matchActivity extends AppCompatActivity {
-
-    public ImageView profilImageView;
-    public TextView nomEntrepriseTextView;
-    public TextView dernierMessageTextView;
-    public TextView dateTextView;
-    private DatabaseReference firebaseDatabase;
-
-
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firestore;
+    String userId;
+    Button disconnectButton, profilButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matchs);
-       // profilImageView = (ImageView) findViewById(R.id.profilImageView);
-        nomEntrepriseTextView = (TextView) findViewById(R.id.nomEntrepriseTextView);
-        dernierMessageTextView = (TextView) findViewById(R.id.dernierMessageTextView);
-        //dateTextView = (TextView) findViewById(R.id.dateTextView);
+        disconnectButton = findViewById(R.id.disconnectButton);
+        profilButton = findViewById(R.id.profilButton);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+        userId = firebaseAuth.getCurrentUser().getUid();
 
-
-        firebaseDatabase = FirebaseDatabase.getInstance().getReference();
-
-        nomEntrepriseTextView.setText(firebaseDatabase.getKey());
-
-        ValueEventListener postListner = new ValueEventListener() {
+        disconnectButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                Log.e("???", String.valueOf(dataSnapshot));
-
-                nomEntrepriseTextView.setText(firebaseDatabase.getKey());
-
+            public void onClick(View view) {
+                finish();
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
             }
+        });
 
+        profilButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e("???", String.valueOf(databaseError));
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), profil.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
             }
-        };
+        });
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        FirebaseAuth.getInstance().signOut();
     }
 }
